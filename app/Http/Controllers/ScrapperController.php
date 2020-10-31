680384e9ee6b2a6cd299568218111cbdd3ae55b9
Benz12445 <benz12445@gmail.com>
2020-10-31 15:35:01 +0700

    update composer and modify scrapper

--- app/Http/Controllers/ScrapperController.php
+++ app/Http/Controllers/ScrapperController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
 
 class ScrapperController extends Controller
 {
     //
+
+    public function test()
+    {
+        $res = Http::get();
+    }
 }
