1049a23b1d37eb890937d7f3a38d2836f7b79a74
Benz12445 <benz12445@gmail.com>
2020-11-04 20:59:34 +0700

    update webpage layout

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,21 +14,21 @@ class ApiController extends Controller
     public function fetchByDate($date)
     {
 
-        $data = $this->fetchDataFromAPIByDate($date,'thailand');
+        // $data = $this->fetchDataFromAPIByDate($date,'thailand');
      
-        return view('main',["body"=>$data]);
+        return view('main');
+        // return view('main',["body"=>$data]);
     }
 
     public function fetchDataFromAPIByDate($date,$country)
     {
         $date = $date ?? Date('Y-m-d',strotime('now'));
         $country = $country ?? 'thailand';
-        
+
         $cl = new \GuzzleHttp\Client();
         $rs = $cl->request('GET','https://api.covid19tracking.narrativa.com/api/'.$date.'/country/'.$country,['Content-type' => 'application/json']);
 
         $body =  $rs->getBody();
-  
         
         $str_html = (string) $body;
         
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,30 +4,53 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            
-        </style>
+        <title>Covid Monitor</title>
+        <script src="/js/app.js"></script>
+        <link href="/css/app.css" rel="stylesheet">
+     
     </head>
     <body>
-       <div class="container">
-            <div class="col-md">
-            {{$body}}
+        <div class="container-fluid nav-container">
+            <aside class="sidebar">
+                <h1>Sidebar header</h1>
+               <ul class="sidebar-ul">
+                    <li class="item">Countries</li>
+                    <li class="item">Statistic</li>
+               </ul>
+            </aside>
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="input-search mr-sm-2" type="search" placeholder="Search">
+                    <button class="input-btn my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+        </div>
+       
+       
+        <div class="container-fluid content">
+            <div class="row">
+                <div class="col-md"><h1>Overview</h1></div>
+             
             </div>
-       </div>
+            <div class="row ">
+                <div class="col-md">
+                    <div class="card-custom">
+                        test
+                    </div>
+                </div>
+                <div class="col-md"><div class="card-custom">
+                        test
+                    </div></div>
+                <div class="col-md"><div class="card-custom">
+                        test
+                    </div></div>
+            </div>
+            
+        
+        </div>
+            
+  
     </body>
+
 </html>
